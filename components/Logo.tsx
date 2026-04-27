5227223671cbca13f86f1cebb315118ee71e0bb7
quraishi31 <muhammadamir.port@gmail.com>
2026-04-27 15:50:55 +0500

    Delete components/Logo.tsx

--- components/Logo.tsx
+++
@@ -1,9 +0,0 @@
-export function Logo({ className = 'h-14 w-auto' }: { className?: string }) {
-  return (
-    <img
-      src={require('../assets/logo.png')}
-      className={className}
-      alt="11th Hour"
-    />
-  )
-}
