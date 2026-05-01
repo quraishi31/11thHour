06441e09881bd20b7a4231821a5a0d57175d82b1
quraishi31 <muhammadamir.port@gmail.com>
2026-05-01 17:20:23 +0500

    Update logo image source path

--- components/Logo.tsx
+++ components/Logo.tsx
@@ -3,7 +3,7 @@ import Image from 'next/image'
 export function Logo({ className = 'h-14 w-auto' }: { className?: string }) {
   return (
     <Image
-      src="/Encore logo.png"
+      src="/Encore.png"
       alt="Encore"
       width={200}
       height={56}
