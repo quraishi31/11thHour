b915e35f9834fdd824f3d89f655081cab65eabca
quraishi31 <muhammadamir.port@gmail.com>
2026-05-02 17:34:54 +0500

    Replace SVG logo with new design

--- components/Logo.tsx
+++ components/Logo.tsx
@@ -4,43 +4,6 @@ export function Logo({ className = 'h-14 w-auto' }: { className?: string }) {
   const gradientId = useId()
 
   return (
-    <svg
-      viewBox="0 0 420 120"
-      fill="none"
-      xmlns="http://www.w3.org/2000/svg"
-      className={className}
-      aria-label="11th Hour"
-      role="img"
-    >
-      <defs>
-        <linearGradient id={gradientId} x1="0%" y1="0%" x2="100%" y2="100%">
-          <stop offset="0%" style={{ stopColor: '#00c2a5', stopOpacity: 1 }} />
-          <stop offset="100%" style={{ stopColor: '#00e0c0', stopOpacity: 1 }} />
-        </linearGradient>
-      </defs>
-
-      <rect x="18" y="22" rx="18" ry="18" width="76" height="76" fill={`url(#${gradientId})`} />
-      <rect x="40" y="38" width="7" height="43" fill="white" />
-      <rect x="58" y="38" width="7" height="43" fill="white" />
-      <path
-        d="M34 68 A28 28 0 0 1 78 68"
-        stroke="white"
-        strokeWidth="3.2"
-        fill="none"
-        strokeLinecap="round"
-      />
-
-      <text
-        x="122"
-        y="64"
-        fontFamily="Sora, Arial, sans-serif"
-        fontSize="38"
-        fontWeight="700"
-        letterSpacing="6"
-        fill="white"
-      >
-        ENCORE
-      </text>
-    </svg>
+   <svg id="Layer_1" data-name="Layer 1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" viewBox="0 0 576 432"><defs><style>.cls-1{fill:url(#linear-gradient);}.cls-2{fill:url(#linear-gradient-2);}.cls-3{fill:url(#linear-gradient-3);}.cls-4{font-size:72px;fill:#231f20;font-family:Fonarto;}</style><linearGradient id="linear-gradient" x1="100.8" y1="251.96" x2="149.94" y2="214.03" gradientTransform="translate(-6.44 -16.08) rotate(-3.62)" gradientUnits="userSpaceOnUse"><stop offset="0" stop-color="#2bc4f3"/><stop offset="0.5" stop-color="#00aeee"/><stop offset="1" stop-color="#0085c1"/></linearGradient><linearGradient id="linear-gradient-2" x1="112.55" y1="277.19" x2="169.96" y2="232.87" gradientTransform="translate(-6.44 -16.08) rotate(-3.62)" gradientUnits="userSpaceOnUse"><stop offset="0" stop-color="#2bc4f3"/><stop offset="0.5" stop-color="#00aeee"/><stop offset="1" stop-color="#0095da"/></linearGradient><linearGradient id="linear-gradient-3" x1="164.45" y1="185.66" x2="184.13" y2="172.9" gradientTransform="matrix(1, 0, 0, 1, 0, 0)" xlink:href="#linear-gradient-2"/></defs><path class="cls-1" d="M116.27,225.25c-.15-1-.26-2-.33-3a31.81,31.81,0,0,1,41.93-32.12C159,199,157.09,219.48,116.27,225.25Z"/><path class="cls-2" d="M179.4,218.27A31.79,31.79,0,0,1,117,228.6c9.11.38,43-.69,46.21-36.06A31.77,31.77,0,0,1,179.4,218.27Z"/><path class="cls-3" d="M164.53,185.76a1,1,0,0,1-.17-.78c.65-3.57,1.47-6.46,1.52-7,0-.1-.15-.16-.19-.06-.49,1.23-1.79,3.53-3,6.09a.81.81,0,0,1-.51.43l-8.23,2.29c-.05,0-.09,0-.05-.08l12.85-12s0,0,0,0l2.33,17.56a0,0,0,0,1,0,0Z"/><text class="cls-4" transform="translate(195.92 244.13)">ENCORE</text></svg>
   )
 }
